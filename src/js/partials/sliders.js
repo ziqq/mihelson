8f7d62be5b506f4e13c038797e0f8dc0d472beb8
Anton <anthonyustinoff@gmail.com>
2018-11-04 21:50:59 +0400

    fix first screen offset & slider init

--- src/js/partials/sliders.js
+++ src/js/partials/sliders.js
@@ -1,28 +1,61 @@
 //Slick Slider https://kenwheeler.github.io/slick/
-if ($('.js-hero-slider').find('.m-slider__slide').length > 1) {
-    $('.js-hero-slider').slick({
-        arrows: true,
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 400,
-        autoplaySpeed: 5000,
-        dots: true,
-        appendDots: '.hero__slider .m-slider__dots',
-        dots: true,
-        customPaging: function(slider, i) {
-            return i + 1 + '/' + slider.slideCount;
-        },
-        responsive: [
-            {
-                breakpoint: 481,
-                settings: {
-                    dots: false,
-                    arrows: false
-                }
-            }
-        ]
+// if ($('.js-hero-slider').find('.m-slider__slide').length > 1) {
+//     $('.js-hero-slider').slick({
+//         arrows: true,
+//         infinite: true,
+//         autoplay: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         speed: 400,
+//         autoplaySpeed: 5000,
+//         dots: true,
+//         appendDots: '.hero__slider .m-slider__dots',
+//         dots: true,
+//         customPaging: function(slider, i) {
+//             return i + 1 + '/' + slider.slideCount;
+//         },
+//         responsive: [
+//             {
+//                 breakpoint: 481,
+//                 settings: {
+//                     dots: false,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+// }
+
+if ($('.js-hero-slider').length) {
+    console.log('---', 'init');
+    $('.js-hero-slider').each(function() {
+        let $slide = $(this).find('.m-slider__slide');
+        if ($slide.length > 1) {
+            $(this).slick({
+                arrows: true,
+                infinite: true,
+                autoplay: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 400,
+                autoplaySpeed: 5000,
+                dots: true,
+                appendDots: '.hero__slider .m-slider__dots',
+                dots: true,
+                customPaging: function(slider, i) {
+                    return i + 1 + '/' + slider.slideCount;
+                },
+                responsive: [
+                    {
+                        breakpoint: 481,
+                        settings: {
+                            dots: false,
+                            arrows: false
+                        }
+                    }
+                ]
+            });
+        }
     });
 }
 
@@ -44,34 +77,70 @@ var infoSliderSettings = {
     ]
 };
 
-if ($('.js-slider').find('.m-slider__slide').length > 1) {
-    $('.js-slider').slick({
-        infoSliderSettings
+if ($('.js-slider').length) {
+    $('.js-slider').each(function() {
+        let $slide = $(this).find('.m-slider__slide');
+        if ($slide.length > 1) {
+            $(this).slick({
+                infoSliderSettings
+            });
+        }
     });
 }
 
-if ($('.js-jurnal-slider').find('.m-slider__slide').length > 1) {
-    $('.js-jurnal-slider').slick({
-        arrows: true,
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 700,
-        autoplaySpeed: 5000,
-        dots: true,
-        appendDots: '.jurnal-article__slider .m-slider__dots',
-        customPaging: function(slider, i) {
-            return i + 1 + '/' + slider.slideCount;
-        },
-        responsive: [
-            {
-                breakpoint: 481,
-                settings: {
-                    dots: false,
-                    arrows: false
-                }
-            }
-        ]
+if ($('.js-jurnal-slider').length) {
+    $('.js-jurnal-slider').each(function() {
+        let $slide = $(this).find('.m-slider__slide');
+        if ($slide.length > 1) {
+            $(this).slick({
+                arrows: true,
+                infinite: true,
+                autoplay: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                speed: 700,
+                autoplaySpeed: 5000,
+                dots: true,
+                appendDots: '.jurnal-article__slider .m-slider__dots',
+                customPaging: function(slider, i) {
+                    return i + 1 + '/' + slider.slideCount;
+                },
+                responsive: [
+                    {
+                        breakpoint: 481,
+                        settings: {
+                            dots: false,
+                            arrows: false
+                        }
+                    }
+                ]
+            });
+        }
     });
 }
+
+// if ($('.js-jurnal-slider').find('.m-slider__slide').length > 1) {
+//     $('.js-jurnal-slider').slick({
+//         arrows: true,
+//         infinite: true,
+//         autoplay: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         speed: 700,
+//         autoplaySpeed: 5000,
+//         dots: true,
+//         appendDots: '.jurnal-article__slider .m-slider__dots',
+//         customPaging: function(slider, i) {
+//             return i + 1 + '/' + slider.slideCount;
+//         },
+//         responsive: [
+//             {
+//                 breakpoint: 481,
+//                 settings: {
+//                     dots: false,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+// }
